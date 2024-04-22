945be1cef80b6d397601dbd9fedc72a7431f94ef
Thiago Oliveira <thiago.oliveira.ds18@gmail.com>
2024-04-22 18:19:11 -0300

    Feat: Date Formated

--- frontend/src/components/ButtonMedicine/index.tsx
+++ frontend/src/components/ButtonMedicine/index.tsx
@@ -10,15 +10,22 @@ interface Props{
 }
 
 export default function ButtonMedicine({id, name, company, published_at} : Props){
-    
+
     const [modal, setModal] = useState(false);
+
+    function formatedDate(date: string) {
+        const formated_dt = date;
+        const newDate = new Date(formated_dt);
+        const x = `${newDate.getDay()}/${newDate.getMonth()}/${newDate.getFullYear()} - ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`;
+        return x;
+    }
 
     return(
         <>
             <button className="button-container" onClick={() => setModal(!modal)}>
                 <h1 className="button-name button-text">{name}</h1>
                 <h2 className="button-company button-text">{company}</h2>
-                <h3 className="button-published button-text">{published_at}</h3>
+                <h3 className="button-published button-text">{formatedDate(published_at)}</h3>
             </button>
             {
                 modal && (
